0da1ca6da86a28fa79825462343449c5efa93b78
Deepak Kumar <dk4503835@gmail.com>
2025-07-27 16:57:07 +0530

    using Stack print value and delete value

--- week1/collectionss.java
+++ week1/collectionss.java
@@ -6,18 +6,35 @@ public class collectionss {
 
     public static void main (String args[]){
     
-        List<Integer> a = new  LinkedList<>();
+    /*  List<Integer> a = new  LinkedList<>();
 
         a.add(1);
         a.add(5);
         a.add(10);
 
         System.out.println(a);
-        
+
        // inhence for loop
         for(Integer x : a)
         {
             System.out.println(x);
         }
+    */   
+      
+    Stack<Integer> S = new Stack<>();
+
+    S.push(1);
+    S.push(20);
+    S.push(30);
+
+    System.out.println(S);
+    
+    System.out.println("peek = " +S.peek());
+
+    S.pop();
+
+    System.out.println(S);
+
+
     }
 }
