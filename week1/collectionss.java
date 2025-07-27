1d8c2f779539ea1dd725e26d55fe1ea609c3b713
Deepak Kumar <dk4503835@gmail.com>
2025-07-27 17:19:05 +0530

    using Queue print value and delete value

--- week1/collectionss.java
+++ week1/collectionss.java
@@ -19,22 +19,38 @@ public class collectionss {
         {
             System.out.println(x);
         }
-    */   
+       
       
-    Stack<Integer> S = new Stack<>();
+        Stack<Integer> S = new Stack<>();
 
-    S.push(1);
-    S.push(20);
-    S.push(30);
+        S.push(1);
+        S.push(20);
+        S.push(30);
 
-    System.out.println(S);
-    
-    System.out.println("peek = " +S.peek());
+        System.out.println(S);
+        
+        System.out.println("peek = " +S.peek());
 
-    S.pop();
+        S.pop();
 
-    System.out.println(S);
+        System.out.println(S);
 
+     */
+       // < ye yah bata raha hai ki hamne is Queue me integere type ka value addn kar rahe hai >
+        
+        Queue<Integer> q = new LinkedList<>();
 
+        q.offer(10);
+        q.offer(20);
+        q.offer(30);
+
+        // ye toString ki help se print kar raha hai head to rear like array whitout loop
+        System.out.println(q);
+        System.out.println("peek is :"+ q.peek());
+
+        q.poll();
+        System.out.println(q);
+
+        
     }
 }
